3933fe5b10b73c2ebbed772883dcead9241d453c
Ryan Domingue <ryan@alleyinteractive.com>
2018-01-07 02:57:27 -0800

    scripts: Ensure consistent formatting throughout

--- utils/logger.sh
+++ utils/logger.sh
@@ -3,18 +3,19 @@
 # Logging helpers
 # By Necolas Gallagher
 # https://github.com/necolas/dotfiles/blob/master/lib/utils
+
 log_header() {
-  printf "\n$(tput setaf 7)%s$(tput sgr0)\n" "${@}"
+  printf "\n$(tput setaf 7)%s$(tput sgr0)\n" "${@}";
 }
 
 log_success() {
-  printf "$(tput setaf 64)✓ %s$(tput sgr0)\n" "${@}"
+  printf "$(tput setaf 64)✓ %s$(tput sgr0)\n" "${@}";
 }
 
 log_error() {
-  printf "$(tput setaf 1)x %s$(tput sgr0)\n" "${@}"
+  printf "$(tput setaf 1)x %s$(tput sgr0)\n" "${@}";
 }
 
 log_warning() {
-  printf "$(tput setaf 136)! %s$(tput sgr0)\n" "${@}"
+  printf "$(tput setaf 136)! %s$(tput sgr0)\n" "${@}";
 }
