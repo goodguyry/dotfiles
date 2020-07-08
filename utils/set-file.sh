9482d6e8c421a30c476c07d52d3bb17abcc5506d
Ryan Domingue <rychdom+github@gmail.com>
2020-07-07 22:03:15 -0700

    Adds options and correct conditionals
    Adds --skip-git-init; renames --no-packages to --skip-packages

--- utils/set-file.sh
+++ utils/set-file.sh
@@ -5,7 +5,7 @@
 function set_file() {
   local SRC="${DOTFILES_DIRECTORY}/${1}";
   local DEST="$2";
-  if [[ $COPY_FILES ]]; then
+  if $COPY_FILES; then
     # Copy with rsync
     rsync -avz --quiet "${SRC}" "${DEST}";
   else
