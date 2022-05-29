0ab34e837bdc268b61482b9972e18ae0c3b83b94
Ryan Domingue <rychdom+github@gmail.com>
2022-05-28 19:35:19 -0700

    Removes unused applications

--- var/packages.macos.sh
+++ var/packages.macos.sh
@@ -40,47 +40,35 @@ declare -a CASK_APPS=(
   atext
   bartender
   betterzip
-  caffeine
   chronosync
   clamxav
   cleanshot
-  clipy
   coconutbattery
   daisydisk
   dropbox
   firefox
   firefox-developer-edition
-  gitify
   google-chrome
   google-chrome-canary
   handbrake
   hazel
   imageoptim
-  macdown
   macmediakeyforwarder
-  muzzle
   notion
   onyx
   opera
   opera-beta
   opera-developer
   opera-mobile-emulator
-  pixelsnap
   protonvpn
   rar
   safari-technology-preview
   sequel-pro
-  sip
-  sketch
   sublime-merge
   sublime-text
   suspicious-package
   tor-browser
-  transmission
   transmit
-  vagrant
-  vlc
-  zeplin
 );
 
 declare -a QL_PLUGINS=(
@@ -121,15 +109,12 @@ BREW_CASK_LIST=("${CASK_APPS[@]}" "${QL_PLUGINS[@]}" "${FONTS[@]}");
 declare -a MAS_APPS_LIST=(
   1091189122 # Bear
   414209656  # Better Rename 9
-  420212497  # Byword
-  777886035  # Duo
   682658836  # GarageBand
   1568924476 # Mela
   409203825  # Numbers
   409201541  # Pages
   407963104  # Pixelmator
   1529448980 # Reeder 5
-  803453959  # Slack
   1006087419 # SnippetsLab
   425424353  # The Unarchiver
   904280696  # Things
