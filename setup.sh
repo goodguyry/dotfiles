2dc0286ada6368c150bf8832f6e3dfe6ef09f3c9
Ryan Domingue <rychdom+github@gmail.com>
2024-01-20 00:04:33 -0800

    Uses DOTFILES_ROOT for sourcing files
    Rather than symlinking them to the home directory

--- setup.sh
+++ setup.sh
@@ -4,41 +4,14 @@
 # Report bugs at: https://github.com/goodguyry/dotfiles/issues
 #
 # This is free software with ABSOLUTELY NO WARRANTY.
+#
+# Adds the dotfiles command.
 
-# Create required directories.
-if [[ "function" == "$(type -t mmkd)" ]]; then
-  mmkd "${HOME}/.bin";
-else
-  [[ ! -d "${HOME}/.bin" ]] && mkdir -pv "${HOME}/.bin";
-fi
+# Source setup files.
+source "./home/functions/mmkd";
+source "./lib/status";
 
-# Add the dotfiles command.
+mmkd "${HOME}/.bin" && status "Created '${HOME}/.bin'";
 ln -sf "$(realpath dotfiles)" "${HOME}/.bin/dotfiles";
 
-# @todo Synking these may be unnecessary, since we can reference DOTFILES_ROOT?
-
-source "$(realpath lib/setfile)";
-
-if [[ "function" == "$(type -t mmkd)" ]]; then
-  mmkd "${HOME}/.dotfiles/lib";
-else
-  [[ ! -d "${HOME}/.dotfiles/lib" ]] && mkdir -pv "${HOME}/.dotfiles/lib";
-fi
-
-# Sync sourced files.
-for lib_file in lib/*; do
-  [[ -f "${lib_file}" ]] && setfile "${lib_file}" "${HOME}/.dotfiles/lib/$(basename $lib_file)";
-done;
-
-if [[ "function" == "$(type -t mmkd)" ]]; then
-  mmkd "${HOME}/.dotfiles/bin";
-else
-  [[ ! -d "${HOME}/.dotfiles/bin" ]] && mkdir -pv "${HOME}/.dotfiles/bin";
-fi
-
-# Sync bin files.
-for bin_file in bin/*; do
-  [[ -f "${bin_file}" ]] && setfile "${bin_file}" "${HOME}/.dotfiles/bin/$(basename $bin_file)";
-done;
-
-[ -n "$(type -P dotfiles)" ] && printf "%s: Done. Type 'dotfiles' to continue.\n" "$(basename "${BASH_SOURCE[0]}")";
+[ -n "$(type -P dotfiles)" ] && status --success "Done. Type 'dotfiles' to continue.";
