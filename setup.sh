40ca69fec26e5a371c474a450ab4fc9f4360ee67
Ryan Domingue <rychdom+github@gmail.com>
2024-02-05 21:06:09 -0800

    Syncs subcommand files to ~/.dotfiles
    This will allow other projects to add subcommands

--- setup.sh
+++ setup.sh
@@ -16,8 +16,18 @@ DOTFILES_ROOT=$(dirname "$(realpath "${BASH_SOURCE[0]}")");
 # Source setup files.
 source "${DOTFILES_ROOT}/lib/mmkd";
 source "${DOTFILES_ROOT}/lib/status";
+source "${DOTFILES_ROOT}/lib/setfile";
 
 mmkd "${HOME}/.bin" && status "Created '${HOME}/.bin'";
 ln -sf "${DOTFILES_ROOT}/dotfiles" "${HOME}/.bin/dotfiles";
+
+mmkd "${HOME}/.dotfiles";
+if [[ $? ]]; then
+  for subcommand_file in "${DOTFILES_ROOT}"/bin/*; do
+    [[ -f "${subcommand_file}" ]] && setfile "${subcommand_file}" "${HOME}/.dotfiles/$(basename "${subcommand_file}")";
+  done;
+else
+  status --error "Could not create ${HOME}/.dotfiles";
+fi
 
 [ -n "$(type -P dotfiles)" ] && status --success "Done. Type 'dotfiles' to continue.";
